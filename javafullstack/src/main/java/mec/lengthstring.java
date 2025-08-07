899312acedb78483f929d34627e805b57c13fe07
Abi395 <abinayas136@gmail.com>
2025-08-07 12:03:46 +0530

    Update some files

---
+++ javafullstack/src/main/java/mec/lengthstring.java
@@ -0,0 +1,19 @@
+package mec;
+
+
+import java.util.Scanner;
+
+public class lengthstring {
+
+    public static int getstringlength(String length){
+        return length.length();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String Input = sc.nextLine();
+        int length = getstringlength(Input);
+        System.out.println("Length of the string:" +length);
+        sc.close();
+    } 
+}
